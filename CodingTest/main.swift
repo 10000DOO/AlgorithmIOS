118359dde3fe1c6f14b6bcbb2214ff35d7aa7dac
이건준 <gunjoun99@gmail.com>
2023-08-25 15:46:17 +0900

    feat : 백준 2217 그리디

--- CodingTest/main.swift
+++ CodingTest/main.swift
@@ -4,8 +4,28 @@
 //
 //  Created by 10000DOO on 2023/08/19.
 //
+// readLine()!.split(separator: " ")
+// readLine()!.split(separator: " ").map{ Int(String($0))! }
+//var input = readLine()!
 
 import Foundation
 
-print("Hello, World!")
+var N = Int(readLine()!)!
+var kg = [Int]()
 
+for _ in 0..<N {
+    kg.append(Int(readLine()!)!)
+}
+
+kg.sort(by: >)
+
+var max = 0;
+
+for i in 0..<kg.count {
+    var weight = kg[i] * (i+1)
+    if weight > max {
+        max = weight
+    }
+}
+
+print(max)
