bbdc620770dbf7fff1088818d129cc338c20f983
10000DOO <57493546+10000DOO@users.noreply.github.com>
2023-09-28 15:53:05 +0000

    Update 행렬.swift

--- 백준/Silver/1080. 행렬/행렬.swift
+++ 백준/Silver/1080. 행렬/행렬.swift
@@ -1,56 +1,75 @@
 import Foundation
 
-let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nm[0]
-let m = nm[1]
-var a = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
-var b = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
-var isSame = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
-var result = 0
-
-for i in 0..<n {
-    a[i] = readLine()!.map { Int(String($0))! }
-}
-
-for i in 0..<n {
-    b[i] = readLine()!.map { Int(String($0))! }
-}
-
-if a == b {
-    print(0)
-    exit(0)
-}
-
-if (n < 3 || m < 3) {
-    print(-1)
-    exit(0)
-}
-
-for i in 0..<n {
-    for j in 0..<m {
-        isSame[i][j] = a[i][j] == b[i][j]
+func main() {
+    let NMArr = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    let N = NMArr[0]
+    let M = NMArr[1]
+    var count = 0
+    
+    var input = Array(repeating: [Int](), count: N)
+    var target = Array(repeating: [Int](), count: N)
+    var matching = Array(repeating: [Bool](), count: N)
+    
+    for i in 0..<N {
+        let strArr = Array(readLine()!)
+        let intArr = strArr.map { Int(String($0))! }
+        input[i] = intArr
     }
-}
-
-for i in 0..<n-2 {
-    for j in 0..<m-2 {
-        if isSame[i][j] { continue }
-        for k in 0..<3 {
-            for l in 0..<3 {
-                isSame[i+k][j+l].toggle()
+    
+    for i in 0..<N {
+        let strArr = Array(readLine()!)
+        let intArr = strArr.map { Int(String($0))! }
+        target[i] = intArr
+        for j in 0..<M {
+            if input[i][j] == target[i][j] {
+                matching[i].append(true)
+            } else {
+                matching[i].append(false)
             }
         }
-        result += 1
     }
-}
+    
+    if input == target {
+        print(0)
+        exit(0)
+    }
 
-print(checkPossibility() ? result : -1)
-
-func checkPossibility() -> Bool {
-    for i in 0..<n {
-        for j in 0..<m {
-            if !isSame[i][j] { return false }
+    if (N < 3 || M < 3) {
+        print(-1)
+        exit(0)
+    }
+    
+    for i in 0..<N - 2 {
+        for j in 0..<M - 2 {
+            if matching[i][j] == false {
+                matching[i][j] = true
+                matching[i][j+1] = !matching[i][j+1]
+                matching[i][j+2] = !matching[i][j+2]
+                matching[i+1][j] = !matching[i+1][j]
+                matching[i+1][j+1] = !matching[i+1][j+1]
+                matching[i+1][j+2] = !matching[i+1][j+2]
+                matching[i+2][j] = !matching[i+2][j]
+                matching[i+2][j+1] = !matching[i+2][j+1]
+                matching[i+2][j+2] = !matching[i+2][j+2]
+                count += 1
+            }
+        }
+        if i != N-3 && (matching[i][M-2] == false || matching[i][M-1] == false) {
+            print("-1")
+            exit(0)
+        }
+        if i == N-3 {
+            if matching[i][M-2] == false || matching[i][M-1] == false {
+                print("-1")
+                exit(0)
+            }
+            if matching[N-1].contains(false) || matching[N-2].contains(false) {
+                print("-1")
+                exit(0)
+            }
         }
     }
-    return true
+    print(count)
 }
+
+main()
