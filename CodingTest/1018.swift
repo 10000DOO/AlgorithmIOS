1019a5faa40e8e14c63c698c069097cf627c70a2
10000DOO <gunjoun99@gmail.com>
2023-11-16 01:30:22 +0900

    Terminal feat : 백준 1018 완탐 다시

--- CodingTest/1018.swift
+++ CodingTest/1018.swift
@@ -7,40 +7,42 @@
 
 import Foundation
 
-let size = readLine()!.split(separator: " ").map{ Int(String($0))! }
+let arr = readLine()!.split(separator: " ").map{ Int(String($0))! }
+let height = arr[0]
+let width = arr[1]
 var board = [[String]]()
-var minNum = Int.max
+var result = Int.max
 
-for _ in 0..<size[0] {
+for _ in 0..<height {
     let bw = Array(readLine()!)
     board.append(bw.map{ String($0) })
 }
 
-for i in 0...(size[0] - 8) {
-    for j in 0...(size[1] - 8) {
-        var countW = 0
-        var countB = 0
-        for k in 0..<8 {
-            for l in 0..<8 {
-                if (i + k + j + l) % 2 == 0 {
-                    if board[i + k][j + l] != "W" {
-                        countW += 1
+for i in 0...height - 8 {
+    for j in 0...width - 8 {
+        var Bcount = 0
+        var Wcount = 0
+        for k in 0 + i..<8 + i {
+            for l in 0 + j..<8 + j {
+                if (k + l) % 2 == 0 {
+                    if board[k][l] != "W" {
+                        Wcount += 1
                     }
-                    if board[i + k][j + l] != "B" {
-                        countB += 1
+                    if board[k][l] != "B" {
+                        Bcount += 1
                     }
                 } else {
-                    if board[i + k][j + l] != "B" {
-                        countW += 1
+                    if board[k][l] != "B" {
+                        Wcount += 1
                     }
-                    if board[i + k][j + l] != "W" {
-                        countB += 1
+                    if board[k][l] != "W" {
+                        Bcount += 1
                     }
                 }
             }
         }
-        minNum = min(minNum, min(countW, countB))
+        result = min(result, min(Bcount, Wcount))
     }
 }
 
-print(minNum)
+print(result)
