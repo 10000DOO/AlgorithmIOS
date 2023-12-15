a47520446992d19cf79e8f6ba379b26df58fde51
10000DOO <gunjoun99@gmail.com>
2023-12-15 13:34:08 +0900

    feat : 백준 9655 DP

---
+++ CodingTest/9655.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  CodingTest
+//
+//  Created by 이건준 on 12/15/23.
+//
+
+import Foundation
+
+let N = Int(readLine()!)!
+
+if N % 2 == 0 {
+    print("CY")
+} else {
+    print("SK")
+}
