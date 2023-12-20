30cc94ad133f99ff28f7aae236078e4c3a0cb6db
10000DOO <gunjoun99@gmail.com>
2023-12-20 23:44:14 +0900

    feat : 백준 9655 DP 다시풀기

--- CodingTest/9655.swift
+++ CodingTest/9655.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 let N = Int(readLine()!)!
+var cache = Array(repeating: -1, count: 1001) // 게임 과정 기록.
+cache[1] = 1
+cache[2] = 2
+cache[3] = 1
+cache[4] = 2
 
-if N % 2 == 0 {
-    print("CY")
-} else {
+if N >= 5 {
+    for i in 5...N {
+        cache[i] = min(cache[i - 3], cache[i - 1]) + 1
+    }
+}
+
+if cache[N] % 2 == 1 { // 게임한 횟수가 홀수면,
     print("SK")
+}
+else { // 게임한 횟수가 짝수면,
+    print("CY")
 }
