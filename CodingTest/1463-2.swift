ea89e807923ad0fbc4bc09dbe55491da62896782
10000DOO <gunjoun99@gmail.com>
2023-12-26 14:21:00 +0900

    feat : 백준 1463 dp

---
+++ CodingTest/1463-2.swift
@@ -0,0 +1,38 @@
+//
+//  1463-2.swift
+//  CodingTest
+//
+//  Created by 이건준 on 12/26/23.
+//
+
+import Foundation
+
+var N = Int(readLine()!)!
+
+var arr = Array(repeating: 0, count: N + 1)
+
+if N >= 2 {
+    arr[2] = 1
+}
+if N >= 3 {
+    arr[3] = 1
+}
+if N >= 4 {
+    arr[4] = 2
+}
+
+if N > 4 {
+    for i in 5...N {
+        arr[i] = arr[i - 1] + 1
+        
+        if i % 3 == 0 {
+            arr[i] = min(arr[i], arr[i/3] + 1)
+        }
+        
+        if i % 2 == 0 {
+            arr[i] = min(arr[i], arr[i/2] + 1)
+        }
+    }
+}
+
+print(arr[N])
