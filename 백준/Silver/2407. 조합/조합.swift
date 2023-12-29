9fb56d79d1ccdbff341806e3c9f17418050991dc
10000DOO <57493546+10000DOO@users.noreply.github.com>
2023-12-29 15:13:18 +0900

    [Silver III] Title: 조합, Time: 12 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.swift
+++ 백준/Silver/2407. 조합/조합.swift
@@ -1,47 +1,31 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let n = input[0]
-let m = input[1]
+let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = nm[0], m = nm[1]
+let limit: UInt64 = 1000000000000000000
+var left = [[UInt64]](repeating: [UInt64](repeating: 0, count: n + 1), count: n + 1)
+var right = [[UInt64]](repeating: [UInt64](repeating: 0, count: n + 1), count: n + 1)
 
-var dp = Array(repeating: Array(repeating: "0", count: 101), count: 101)
+right[1][0] = 1
+right[1][1] = 1
 
-func calculateCombination(_ n: Int, _ r: Int) -> String {
-    if n == r || r == 0 {
-        return "1"
+for i in 2...n {
+    right[i][0] = 1
+    right[i][i] = 1
+
+    for j in 1..<i {
+        right[i][j] = right[i - 1][j - 1] + right[i - 1][j]
+        left[i][j] = left[i - 1][j - 1] + left[i - 1][j]
+
+        if right[i][j] >= limit {
+            right[i][j] -= limit
+            left[i][j] += 1
+        }
     }
-    
-    if dp[n][r] != "0" {
-        return dp[n][r]
-    }
-    
-    dp[n][r] = bigNumberAddition(calculateCombination(n - 1, r - 1), calculateCombination(n - 1, r))
-    return dp[n][r]
 }
 
-func bigNumberAddition(_ num1: String, _ num2: String) -> String {
-    var tempNum1 = Array(num1.reversed())
-    var tempNum2 = Array(num2.reversed())
-    while tempNum1.count < tempNum2.count {
-        tempNum1.append("0")
-    }
-    while tempNum1.count > tempNum2.count {
-        tempNum2.append("0")
-    }
-    
-    var carry = 0
-    var result = ""
-    for i in 0..<tempNum1.count {
-        let temp = Int(String(tempNum1[i]))! + Int(String(tempNum2[i]))! + carry
-        result = String(temp % 10) + result
-        carry = temp / 10
-    }
-    
-    if carry != 0 {
-        result = String(carry) + result
-    }
-    
-    return result
+if left[n][m] > 0 {
+    print("\(left[n][m])" + "\(right[n][m])")
+} else {
+    print(right[n][m])
 }
-
-print(calculateCombination(n, m))
