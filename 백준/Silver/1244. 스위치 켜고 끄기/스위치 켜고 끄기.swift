4e348dbe2ff4427f04ec1d3291b4276d2a1c8372
10000DOO <57493546+10000DOO@users.noreply.github.com>
2024-01-04 22:58:04 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.swift
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.swift
@@ -4,16 +4,18 @@ let N = Int(readLine()!)!
 var switchArr = readLine()!.split(separator: " ").map{ Int(String($0))! }
 let member = Int(readLine()!)!
 
-for _ in 0..<member {
+for i in 0..<member {
     let memberCard = readLine()!.split(separator: " ").map{ Int(String($0))! }
-    if memberCard[0] == 1 { // 남자 학생
-        for j in stride(from: memberCard[1], to: N+1, by: memberCard[1]) {
-            switchArr[j-1] = switchArr[j-1] == 0 ? 1 : 0
+    if memberCard[0] == 1 {
+        for j in 0..<N {
+            if (j + 1) % memberCard[1] == 0 {
+                switchArr[j] = switchArr[j] == 0 ? 1 : 0
+            }
         }
-    } else { // 여자 학생
+    } else {
         let mark = memberCard[1] - 1
         switchArr[mark] = switchArr[mark] == 0 ? 1 : 0
-
+        
         var leftIndex = mark - 1
         var rightIndex = mark + 1
         while leftIndex >= 0 && rightIndex < N {
